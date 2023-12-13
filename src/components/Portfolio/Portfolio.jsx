3423a5589cd5a21688bff2a74d5f9d47f8524202
Marchishe <markovolchenko@gmail.com>
2023-12-13 16:03:30 +0200

    added first project for the Portfolio.jsx page

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+import style from './portfolio.module.scss'
+import {items} from "./portfolio.data";
+
 const Portfolio = () => {
-    return (<div>Portfolio</div>)
+    return (
+        <div className={style.project_list}>
+            {items.map((item =>
+                <div key={item.id} className={style.project_item}>
+                    <a href="https://marchishe.github.io/todo_list/" target='_blank'><img src={item.src} alt="project-image"/></a>
+                </div>
+                ))}
+        </div>
+    )
 }
 
 export default Portfolio
---
+++ src/components/Portfolio/portfolio.data.js
@@ -0,0 +1,7 @@
+import todoList from '../../images/todo-list-img.png'
+export const items = [
+    {
+        id: 1,
+        src: todoList
+    }
+]
